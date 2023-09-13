80b39d88df387cf2e3341978dca5c0eb82802998
LoTwT <709937065@qq.com>
2023-09-13 13:48:16 +0800

    chore: init

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { URL, fileURLToPath } from "node:url"
+import { defineConfig } from "vitest/config"
+import AutoImport from "unplugin-auto-import/vite"
+
+export default defineConfig({
+  plugins: [
+    AutoImport({
+      imports: ["vitest"],
+    }),
+  ],
+  test: {
+    includeSource: ["src/*"],
+  },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+})
